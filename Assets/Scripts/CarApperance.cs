8095e4b8c57f61235c3a3d31040890b91a63b628
martaP-beep <marta.ios.developer@gmail.com>
2025-04-07 18:04:17 +0200

    m1

--- Assets/Scripts/CarApperance.cs
+++ Assets/Scripts/CarApperance.cs
@@ -15,6 +15,25 @@ public class CarApperance : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(playerNumber == 0)
+        {
+            playerName = PlayerPrefs.GetString("PlayerName");
+            carColor = ColorCar.IntToColor(
+                PlayerPrefs.GetInt("Red"),
+                PlayerPrefs.GetInt("Green"),
+                PlayerPrefs.GetInt("Blue")
+                );
+        }
+        else
+        {
+            playerName = "Random " + playerNumber;
+            carColor = new Color(
+                Random.Range(0,255),
+                Random.Range(0,255),
+                Random.Range(0,255)
+                );
+        }
+
         nameText.text = playerName;
         carRendrer.material.color = carColor;
         nameText.color = carColor;
--- Assets/Scripts/RaceLauncher.cs
+++ Assets/Scripts/RaceLauncher.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
 
-public class RaceLauncher : MonoBehaviour
+public class RaceLauncher : MonoBehaviourPunCallbacks
 {
     public InputField playerName;
+
+    bool isConnecting;
+    byte maxPlayerPerRoom = 3;
+    string gameVersion = "1";
+    public Text networkText;
 
     public void SetName(string name)
     {
@@ -14,8 +21,10 @@ public class RaceLauncher : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
         if (PlayerPrefs.HasKey("PlayerName"))
         {
             playerName.text = PlayerPrefs.GetString("PlayerName");
@@ -32,4 +41,55 @@ public class RaceLauncher : MonoBehaviour
     {
         
     }
+
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting) {
+            networkText.text += " OnConnectedToMaster \n";
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        networkText.text += " OnJoinRandomFailed \n";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { 
+            MaxPlayers = maxPlayerPerRoom });
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        networkText.text += " OnDisconnected: " + cause +" \n";
+        isConnecting = false;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        networkText.text += " OnJoinedRoom:"+ 
+            PhotonNetwork.CurrentRoom.PlayerCount +" players\n";
+
+        PhotonNetwork.LoadLevel("TestTrack");
+
+    }
+
+    public void ConnectNetwork()
+    {
+        networkText.text = "";
+        isConnecting=true;
+        PhotonNetwork.NickName = playerName.text;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            networkText.text += "Join room \n";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            networkText.text += "Connecting \n";
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+    }
+
 }
